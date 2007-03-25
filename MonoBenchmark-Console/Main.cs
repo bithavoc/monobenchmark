49c798405281029c7f38ff2e90adddf83778a784
thepumpkin1979 <thepumpkin1979@e072024c-502c-0410-b9a7-3532ea87ca88>
2007-03-25 23:14:19 +0000

    Console Project Added

---
+++ MonoBenchmark-Console/Main.cs
@@ -0,0 +1,13 @@
+// project created on 3/25/2007 at 7:12 PM
+using System;
+
+namespace MonoBenchmarkConsole
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
