b2377d2f1607b56e869396960205a5104199c113
thepumpkin1979 <thepumpkin1979@e072024c-502c-0410-b9a7-3532ea87ca88>
2007-03-25 23:48:40 +0000

    TestMethodInfo

--- trunk/MonoBenchmark-Console/Main.cs
+++ trunk/MonoBenchmark-Console/Main.cs
@@ -1,13 +1,31 @@
 // project created on 3/25/2007 at 7:12 PM
 using System;
+using MonoBenchmark;
+using MonoBenchmark.Core;
+using MonoBenchmark.Framework;
 
 namespace MonoBenchmarkConsole
 {
 	class MainClass
 	{
+		static void bg(string val)
+		{
+			Console.WriteLine(val);
+		}
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			bg("Creating Session");
+			
+			TestSession session = new TestSession();
+			session.LoadFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+			bg("Session Created");
+			bg("Session Has Fixtures?=" + session.HasFixtures);
+			
+			
+			
+			
+			bg("Ending Session");
+			
 		}
 	}
 }
