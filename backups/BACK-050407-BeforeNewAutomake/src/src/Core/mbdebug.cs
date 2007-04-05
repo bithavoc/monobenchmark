19ee0bc497cd5f994fd476f19dabc53fdd0dbd69
thepumpkin1979 <thepumpkin1979@e072024c-502c-0410-b9a7-3532ea87ca88>
2007-04-05 20:57:11 +0000

    new Automake Backup

---
+++ backups/BACK-050407-BeforeNewAutomake/src/src/Core/mbdebug.cs
@@ -0,0 +1,21 @@
+
+using System;
+
+namespace MonoBenchmark.Core
+{
+//#if DEBUG
+	public class debug
+	{
+		
+		public debug()
+		{
+		}
+		
+		[System.Diagnostics.ConditionalAttribute("DEBUG")]
+		public static void writeln(string format,params object[] @params)
+		{
+			Console.WriteLine(format,@params);
+		}
+	}
+//#endif
+}
